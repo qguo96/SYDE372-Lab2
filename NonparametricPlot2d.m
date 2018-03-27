1ef9811bb35c7324785c72408762666bf54a8604
Chenlei Shen <shenchenlei@gmail.com>
2018-03-27 12:21:11 -0400

    whoops

--- NonparametricPlot2d.m
+++ NonparametricPlot2d.m
@@ -1,4 +1,4 @@
-function [ output_args ] = NonparametricPlot2d( input_args )
+function NonparametricPlot2d( al_x, al_y, Mesh_Grid_Plot_2, al, bl, cl )
 %NONPARAMETRICPLOT2D Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -10,13 +10,10 @@ hold on
 scatter(cl(:,1), cl(:,2))
 hold on
 
-contour(al_x, al_y, boundary);
+contour(al_x, al_y, Mesh_Grid_Plot_2, 'color', 'k');
 title('Non Parametric Estimation - Parzen Method');
 legend('al datapoints','bl datapoints','cl datapoints');
 xlabel('x axis');
 ylabel('y axis');
 
-contour(al_x, al_y, boundary);
-
 end
-
